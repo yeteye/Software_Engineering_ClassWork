242dd59edac0fc1e39bfe28d404ccdce6b1b4fde
Osprey <u202211707@hust.edu.cn>
2024-05-10 09:03:50 +0800

    更改

--- PetShow.py
+++ PetShow.py
@@ -1,7 +1,14 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QMovie
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout ,QLCDNumber ,QSizePolicy
 from PySide6.QtCore import QSize
+import json
+
+# 打开并读取json文件
+with open('profile.json', 'r') as f:
+    data = json.load(f)
+
 class GIFWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -23,5 +30,38 @@ class GIFWindow(QWidget):
         layout = QVBoxLayout(self)
         layout.addWidget(self.gif_label)
         self.setLayout(layout)
+
+    #def mousePressEvent(self, event):
+
+
+    def count_exp(self,data):
+        self.level = data['level']
+        self.exp = data['exp']
+        if self.exp>=10 :
+            self.level += 1
+            exp = 0
+
+
+        self.cexp.connect(self.update_display)
+
+        layout = QVBoxLayout(self)
+
+        self.lcd = QLCDNumber()
+        self.lcd.setSizePolicy(
+            QSizePolicy.Expanding, QSizePolicy.Expanding
+        )  # 设置 LCD 数字显示的大小策略为尽可能扩展
+        self.lcd.setDigitCount(5)  # 设置显示位数
+        self.lcd.display("00:00")  # 初始显示值为00:00
+        layout.addWidget(self.lcd)
+
+        display_text = f"{self.level:02d}:{self.exp:02d}"
+        self.lcd.display(display_text)
+
+        verticalSpacer = QtWidgets.QSpacerItem(
+            10, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding
+        )
+        layout.addItem(verticalSpacer)
+
+
 
 
