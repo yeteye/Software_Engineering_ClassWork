c17be86081d6f364e8b0b4fbec2978b294310b78
ve1i0na <1907615595@qq.com>
2024-05-11 11:49:55 +0800

    update_list_widget

--- PomodoroWindowGenerator.py
+++ PomodoroWindowGenerator.py
@@ -18,10 +18,13 @@ class PomodoroWindowGenerator(QWidget):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.profile = self.loadProfile()
+        self.tasklist = {}
+        self.tasklist = self.loadTask()
         self.avatarPath = self.profile['avatar']
         self.ui.avatar.setPixmap(QPixmap(self.avatarPath))
         self.ui.avatar.mousePressEvent = self.changeAvatar
         self.ui.TaskCreator.mousePressEvent = self.createTaskUI
+        self.ui.listWidget.itemDoubleClicked.connect(self.onItemDoubleClicked)
 
     ##更改头像
     def changeAvatar(self, mouseEvent: QMouseEvent):
@@ -54,6 +57,16 @@ class PomodoroWindowGenerator(QWidget):
         profileFile = open("profile.json")
         profile = json.load(profileFile)
         profileFile.close()
+        return profile
+
+    def updateProfile(self, item, value):
+        profileFile = open("profile.json", "w")
+        profile = self.profile
+        profile[item] = value
+        profileFile.write(json.dumps(profile, indent=4))
+        profileFile.close()
+
+    def loadTask(self):
         try:
             tasklistFile = open("tasklist.json")
         except FileNotFoundError:
@@ -66,18 +79,24 @@ class PomodoroWindowGenerator(QWidget):
             task = Task(key, tasklist[key])
             self.AddTaskToList(task)
         tasklistFile.close()
-
-        return profile
-
-    def updateProfile(self, item, value):
-        profileFile = open("profile.json", "w")
-        profile = self.profile
-        profile[item] = value
-        profileFile.write(json.dumps(profile, indent=4))
-        profileFile.close()
+        return tasklist
 
     def AddTaskToList(self, task):
         self.ui.listWidget.addItem(task.name)
+        self.tasklist.update({task.name: task.timeLast})
         self.ui.listWidget.repaint()
         return
 
+    def onItemDoubleClicked(self, item):
+        # 获取双击的项，并将其传递给 SendTime 方法
+        task_name = item.text()
+        self.SendTime(task_name)
+
+    def SendTime(self, item):
+        if self.ui.clock.start_button.isEnabled():
+            self.ui.clock.timer.stop()
+            self.ui.clock.stop_button.setEnabled(False)
+            self.ui.clock.flag = 3
+            self.ui.clock.timeLast = self.tasklist[item]
+            self.ui.clock.LcdDisplay(self.tasklist[item])
+        return
