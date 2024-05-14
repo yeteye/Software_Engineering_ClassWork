0aab8a3446356b638e76a8346cc39ba7faead62d
Osprey <u202211707@hust.edu.cn>
2024-05-14 16:58:34 +0800

    changed

--- ExpShow.py
+++ ExpShow.py
@@ -1,6 +1,7 @@
 from PySide6.QtCore import Qt
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QSizePolicy, QLabel, QVBoxLayout, QWidget, QProgressBar
+from PySide6.QtGui import QFont
 import json
 
 class Exp_Show(QWidget):
@@ -8,8 +9,13 @@ class Exp_Show(QWidget):
         super().__init__(parent)
         self.filename = "profile.json"
         self.data = self.load_data()
-        self.display_text = (f"<br><br>LV：{self.data['level']}"
-                             f"<br>EXP：{self.data['exp']}")
+
+        self.display_text = ("""
+            <br><br><span style="color: blue;">LV: </span>
+            <span style="color: green;">""" + f"{self.data['level']}" + """</span>
+            <br><span style="color: blue;">Exp：</span>
+            <span style="color: green;">""" + f"{self.data['exp']}" + """</span>
+                """)
 
         layout = QVBoxLayout(self)
 
@@ -18,28 +24,49 @@ class Exp_Show(QWidget):
         #文本左对齐
         self.label.setAlignment(Qt.AlignLeft)
         #设置字体大小
-        font = self.label.font()
+        font = QFont()
         font.setPointSize(20)  # 设置字体大小为 20
+
+        #设置字体外观
+
+        #self.label.setStyleSheet("color: green;")
+        font.setFamily('Comic Sans MS')
+        font.setBold(True)  # 设置字体粗细
+        font.setItalic(True)  # 设置斜体
+        font.setUnderline(False)  # 添加下划线
+        font.setLetterSpacing(QFont.AbsoluteSpacing, 2)  # 设置字间距
+
         self.label.setFont(font)
-        #设置字体颜色
-        self.label.setStyleSheet("color: green;")
+
         # 创建一个进度条对象
         self.progress_bar = QProgressBar()
         # 设置进度条的范围和初始值
         self.SetProgressBar()
         #进度条外观设置
-        self.progress_bar.setStyleSheet(
-            "QProgressBar {"
-            "    border: 2px solid grey;"
-            "    border-radius: 5px;"
-            "    background-color: lightgrey;"
-            "    text-align: center;"  # 让显示的文本居中对齐
-            "}"
-            "QProgressBar::chunk {"
-            "    background-color: green;"
-            "    width: 20px;"  # 设置进度块宽度
-            "}"
-        )
+
+        #可根据属性更改
+        self.setStyleSheet("""
+                    QProgressBar {
+                        border: 3px solid #FFD700;  /* 金色边框 */
+                        border-radius: 10px;  /* 圆角 */
+                        background-color: #FFA07A;  /* 浅橙色背景 */
+                        height: 15px;  /* 高度 */
+                        text-align: center;  /* 文本居中 */
+                        font-size: 18px;  /* 字体大小 */
+                        color: white;  /* 字体颜色 */
+                        padding: 5px;  /* 内边距 */
+                        box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);  /* 阴影效果 */
+                    }
+                    QProgressBar::chunk {
+                        background-color: qlineargradient(
+                            x1: 0, y1: 0, x2: 1, y2: 1,
+                            stop: 0 #FF4500,  /* 橙红色 */
+                            stop: 1 #32CD32  /* 黄绿色 */
+                        );
+                        border-radius: 10px;  /* 圆角 */
+                        margin: 1px;  /* 间距 */
+                    }
+                """)
         #对文本大小进行设置
         self.label.setSizePolicy(
             QSizePolicy.Expanding, QSizePolicy.Expanding
@@ -58,10 +85,16 @@ class Exp_Show(QWidget):
     def update_labels(self):
         # 获取等级和经验值
         self.data = self.load_data()
-        print(self.data['exp'])
+
         # 更新 QLabel 显示的文本
-        self.display_text = (f"<br><br>LV：{self.data['level']}"
-                             f"<br>EXP：{self.data['exp']}")
+
+
+        self.display_text = ("""
+    <br><br><span style="color: blue;">LV: </span>
+    <span style="color: green;">"""+f"{self.data['level']}"+"""</span>
+    <br><span style="color: blue;">Exp：</span>
+    <span style="color: green;">"""+f"{self.data['exp']}"+"""</span>
+        """)
         self.label.setText(self.display_text)
         self.SetProgressBar()
 
