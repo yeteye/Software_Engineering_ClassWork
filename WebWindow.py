f0e321f1573cf6620f49018c0913269d7be177da
ve1i0na <1907615595@qq.com>
2024-05-28 17:37:14 +0800

    feat:重写的浏览器右键菜单

--- WebWindow.py
+++ WebWindow.py
@@ -1,11 +1,23 @@
 from PySide6.QtCore import QSize
+from PySide6.QtWebEngineCore import QWebEnginePage
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QMenu
 from PySide6.QtGui import QIcon
 
 
+class PromodoroWebEngineView(QWebEngineView):
+    def contextMenuEvent(self, event):
+        # 只显示刷新项
+        menu = QMenu(self)
+        menu.clear()
+        reload_action = self.page().action(QWebEnginePage.ReloadAndBypassCache)
+        reload_action.setText('刷新')
+        menu.addAction(reload_action)
+        menu.exec_(event.globalPos())
+
+
 class WebWindow(QMainWindow):
-    def __init__(self, if_index=True,task='',task_time=''):
+    def __init__(self, if_index=True, task='', task_time=''):
         super().__init__()
         self.if_index = if_index
         self.task = task
@@ -20,11 +32,11 @@ class WebWindow(QMainWindow):
         icon.addFile(u"image/f3006b49c9f1fc1519d2bf688fc52e70.ico", QSize(), QIcon.Normal, QIcon.Off)
         self.setWindowIcon(icon)
         # 创建Web视图
-        self.webView = QWebEngineView()
+        self.webView = PromodoroWebEngineView()
         self.setCentralWidget(self.webView)
         if self.if_index:
-        # 加载网页
-            url = f'http://127.0.0.1:8000/' # 本地服务器地址，运行时请修改
+            # 加载网页
+            url = f'http://127.0.0.1:8000/'  # 本地服务器地址，运行时请修改
         else:
-            url = f'http://127.0.0.1:8000/post/new/?task={self.task}&task_time={self.task_time}' # 本地服务器地址，运行时请修改
+            url = f'http://127.0.0.1:8000/post/new/?task={self.task}&task_time={self.task_time}'  # 本地服务器地址，运行时请修改
         self.webView.setUrl(url)
