3a678ffd164c70b46ca3a13bf5dcd5d5fd1ba36e
ve1i0na <1907615595@qq.com>
2024-05-28 20:37:59 +0800

    feat:重写的浏览器右键菜单

--- WebWindow.py
+++ WebWindow.py
@@ -11,13 +11,13 @@ class PromodoroWebEngineView(QWebEngineView):
         menu = QMenu(self)
         menu.clear()
         reload_action = self.page().action(QWebEnginePage.ReloadAndBypassCache)
-        reload_action.setText('刷新')
+        reload_action.setText("刷新")
         menu.addAction(reload_action)
         menu.exec_(event.globalPos())
 
 
 class WebWindow(QMainWindow):
-    def __init__(self, if_index=True, task='', task_time=''):
+    def __init__(self, if_index=True, task="", task_time=""):
         super().__init__()
         self.if_index = if_index
         self.task = task
@@ -25,18 +25,23 @@ class WebWindow(QMainWindow):
         self.setUI()
 
     def setUI(self):
-        self.setWindowTitle('Pomodoro Community')
+        self.setWindowTitle("Pomodoro Community")
         self.setGeometry(100, 100, 1050, 600)
         # 添加Icon
         icon = QIcon()
-        icon.addFile(u"image/f3006b49c9f1fc1519d2bf688fc52e70.ico", QSize(), QIcon.Normal, QIcon.Off)
+        icon.addFile(
+            "image/f3006b49c9f1fc1519d2bf688fc52e70.ico",
+            QSize(),
+            QIcon.Normal,
+            QIcon.Off,
+        )
         self.setWindowIcon(icon)
         # 创建Web视图
         self.webView = PromodoroWebEngineView()
         self.setCentralWidget(self.webView)
         if self.if_index:
-        # 加载网页
-            url = f'http://frp-fly.top:53065/' # 本地服务器地址，运行时请修改
+            # 加载网页
+            url = f"http://frp-fly.top:53065/"  # 本地服务器地址，运行时请修改
         else:
-            url = f'http://frp-fly.top:53065/post/new/?task={self.task}&task_time={self.task_time}' # 本地服务器地址，运行时请修改
+            url = f"http://frp-fly.top:53065/post/new/?task={self.task}&task_time={self.task_time}"  # 本地服务器地址，运行时请修改
         self.webView.setUrl(url)
